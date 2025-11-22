0ac48055fcfe5f0a522ca34d111cf5bbd5c4e1b7
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-11-22 21:42:50 +0500

    Day 1

---
+++ Day-1/questions.md
@@ -0,0 +1,62 @@
+q1.
+Print Numbers from 1 to N 
+
+ Input: 5
+
+ Output: 1 2 3 4 5
+
+ ✨ Teaches basic loop construction.
+
+q2.
+Print Numbers from N to 1 without changing the loop condition of above question
+
+Input: 5
+
+Output: 5 4 3 2 1
+
+✨ Thinking creatively and manipulating logic within the loop.
+
+q3.
+Print All Even Numbers from 1 to N
+
+Input: 10
+
+Output: 2 4 6 8 10
+
+✨ Introduces conditional checks inside loops.
+
+q4. 
+Sum of First N Natural Numbers
+
+Input: 5
+
+Output: 15
+
+✨ Encourages cumulative addition and variable usage.
+
+q5.
+Product (Factorial) of N
+
+Input: 5
+
+Output: 120
+
+✨ Reinforces loop control with multiplication logic.
+
+q6.
+Sum of All Even Numbers up to N
+
+Input: 10
+
+Output: 30 (2+4+6+8+10)
+
+✨ Combines loops + condition + accumulation.
+
+q7.
+Print Squares of Numbers from 1 to N
+
+Input: 5
+
+Output: 1 4 9 16 25
+
+✨ Introduces arithmetic pattern generation.
---
+++ Day-1/script.js
@@ -0,0 +1,102 @@
+// ans 1
+// let n = 10;
+// for (let i = 1; i<=n; i++){
+//     console.log(i);
+// }
+
+// ans 2 
+// let n = 10;
+// for (let i = 1; i<=n; i++){
+//     console.log(n - i + 1);
+// }
+
+// ans 3 .
+// let n = 10;
+// for(let i = 1; i<=n; i++){
+//     if (i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+// ans 4.
+// let n = 5;
+// let sum = 0;
+// for (let i = 1; i<=n; i++){
+//     sum = sum + i;
+
+// }
+// console.log("Sum = ", sum);
+
+// SECOND METHOD 
+// optimized way
+// brute force solution
+// console.time()
+// let num4a = 10000000000;
+// let sum = num4a * (num4a + 1) / 2;
+// console.log(sum);
+// console.timeEnd()
+
+// ans 5.
+// let n = 5;
+// let fact = 1;
+// for(let i = 1; i<=n; i++){
+//     fact = fact * i;
+// }
+// console.log("Factorial = ", fact);
+
+// ans 6. 
+// let n = 10;
+// let sum = 0;
+// for(let i = 1; i<=n; i++){
+//     if (i % 2 === 0){
+//         sum = sum + i;
+//     }
+// }
+// console.log("Sum of Even numbers from 1 to n is ", sum);
+
+// ans 7. 
+// let n = 5;
+// for(let i = 1; i<=n; i++){
+//     console.log(i * i);
+// }
+
+// HOME WORK 
+// q1.
+// console.time("test")
+// let n = 30;
+// for(let i = 1; i<=n; i++){
+//     if (i % 3 === 0 && i  % 5 === 0){
+//         console.log(i);
+//     }
+// }
+// console.timeEnd("test");
+
+// q2.
+// let n = 10;
+// let sum = 0;
+// for(let i = 1; i<=n; i++){
+//     if (i % 2 !== 0){
+//         sum+=i;
+//     }
+// }
+// console.log(sum);
+
+// q3.
+// let n = 5;
+// for(let i = 1; i<=n; i++){
+//     console.log(i * i * i);
+// }
+
+// q4. 
+// let n = 20;
+// for(let i = 1; i<=n; i++){
+//         let sqrt = Math.sqrt(i);
+
+//         if (Number.isInteger(sqrt) && i % 2 === 0){
+//             console.log(i);
+//         }
+// }
+
+
+
+
