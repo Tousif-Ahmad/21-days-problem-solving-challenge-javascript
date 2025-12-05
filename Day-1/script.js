f983a3e0d8bd42664237b266bd2abbc82d600b34
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-12-05 19:37:42 +0500

    added more questions

--- Day-1/script.js
+++ Day-1/script.js
@@ -96,7 +96,62 @@
 //             console.log(i);
 //         }
 // }
+// console.time()
+// THIS IS CALLED BRUTE FORCE SOLUTION 
+// IT TAKES 2 SECONDS 
+// let num = 100000000;
+// let sum = 0;
+
+// for (let i = 1; i<=num; i++){
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+// console.timeEnd()
+
+// OPTIMIZED SOLUTION 
+// console.time()
+// let num = 100000000;
+// // let sum = 0;
+// let sum = num * (num + 1) / 2;
+// console.log(sum);
+
+// console.timeEnd()
+
+// let num = 20;
+// for (let i = 1; i<=num; i++){
+//     let root = Math.sqrt(i)
+//     if (i % 2 === 0 &&  Number.isInteger(root)){
+//         console.log(i);
+//     }
+// }
 
 
+// DOM QUESTIONS  (qs)
+// let inputField = document.querySelector(".inputField")
+// let btn = document.querySelector(".btn");
+// let numbers = document.querySelector(".numbers");
+// function generateNumbers(){
+//     let value = Number(inputField.value);
+//     numbers.innerHTML = "";
+//     for(let i = 1; i<=value; i++){
+//         let divElem = document.createElement("div");
+//         divElem.textContent = i;
+//         numbers.append(divElem)
 
+//     }
+//     inputField.value = "";
+// }
+// btn.addEventListener("click", generateNumbers)
 
+// (qs 2)
+const homework1 = (e) => {
+  e.preventDefault();
+  let inputValue = parseInt(e.target[0].value);
+  const displayDiv = document.getElementById("number-div");
+  displayDiv.innerHTML = "";
+  for (let i = 1; i <= inputValue; i++) {
+    if (i % 3 === 0 && i % 5 === 0) displayDiv.innerHTML += `${i} - `;
+  }
+  e.target[0].value = "";
+};
